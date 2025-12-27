ff6267a959dfa7b282772486c91d76667354a360
Akhil Babu <akhilbabu034@gmail.com>
2025-12-27 09:20:02 +0530

    1x2DemuxUpdate

--- 1x2 Demux/1x2DemuxDataFlow.v
+++ 1x2 Demux/1x2DemuxDataFlow.v
@@ -1,6 +1,6 @@
-module demux_1_2_tb;
-  reg inp,s;
-  wire [1:0]out;
-  demux_1_2 dut(.out(out),.s(s),.inp(inp));
-  initial begin
- 
+module demux_1_2(out,s,inp);
+  input s,inp;
+  output [1:0] out;
+  assign out[0] = ~s & inp;
+  assign out[1] = s & inp;
+endmodule
